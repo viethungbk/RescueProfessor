4a30a74e32b76649d0425cec335f68e49e60469a
Thanh Duy Truong <thanhduyhust@gmail.com>
2020-05-20 20:51:04 +0700

    Add map resources

---
+++ Assets/Map Resources/Scripts/RotateXModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateXModel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(new Vector3(Time.deltaTime * 60, 0, 0));
+		
+	}
+}
